fc6317d157d294358f1fe78f7a7a47fe6a5f10dd
Anderson_Godoy <134303770+AndersonGodoy1507@users.noreply.github.com>
2026-09-01 17:06:23 -0300

    Add files via upload

---
+++ 01-09exercicio.py
@@ -0,0 +1,26 @@
+# 2 - Crie um gráfico plot para mostra:
+import matplotlib.pyplot as plt
+import pandas as pd 
+import numpy as np
+
+# 2 - Crie um gráfico plot para mostra:
+
+ano = [2021,2022,2023,2024,2025,2026]
+
+vendas = [10000,2000,30000,10000,5000,20000]
+
+
+plt.figure(figsize=(5,6))
+plt.plot(ano,vendas, marker ='X')
+plt.grid(True)
+plt.show()
+
+# ***3 - Desenvolva em gráfico  barras:***
+
+medias_jose = [10,5,8,9,10,5,4]
+meses = ['feve','març','abril','maio','jun','jul','agos']
+plt.figure(figsize= (7,10))
+plt.bar(medias_jose,meses,)
+
+
+plt.show()
