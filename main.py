37a2e8fa006cacca526513b46549ff2929cbc137
Anderson_Godoy <134303770+AndersonGodoy1507@users.noreply.github.com>
2026-09-03 16:24:39 -0300

    Add files via upload
    sobre aula 12

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-print('Sistema de IMC')
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy as np
 
-altura = float(input('Altura: '))
-peso = float(input('Pese: '))
+dados = pd.read_excel('vendas.xlsx')
 
-imc= peso / (altura ** 2)
-
-
-print(imc)
-
-resultado = imc < 18.5 and ('Abaixo do normal') or imc > 18.5 and imc < 25 and ('Peso normal') or ('Acima do Peso')
-
-print(resultado)
+df = pd.DataFrame(dados)
 
 
+media = (df['Vendas']).mean()
+print (media)
+print('xxxxxxx'*15)
+
+Maior_venda_mes = (df['Vendas']).idxmax()
+ma_venda = df.loc[Maior_venda_mes,'Vendas']
+mes_maior_venda = df.loc[Maior_venda_mes, 'Meses']
+ano_venda = df.loc[Maior_venda_mes,'Ano']
+print(mes_maior_venda,ano_venda,ma_venda)
+
+print('xxxxxxx'*15)
+
+menor_venda = (df['Vendas']).idxmin()
+m_venda = df.loc[menor_venda,'Vendas']
+mes_menos = df.loc[menor_venda,'Meses']
+ano_menos = df.loc[menor_venda,'Ano']
+print(mes_menos,ano_menos,m_venda)
+
+
+sns.barplot(data=df, x="Vendas", y="Meses")
+plt.show()
+
+print('xxxxxxx'*15)
